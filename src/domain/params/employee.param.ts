bffbcad55832cb38b2d2017966bac70ecd5cb406
Markus Füreder <markus.fuereder@car-cutter.com>
2024-04-10 09:22:02 +0200

    feat: :necktie: Implement models and params.

--- src/domain/params/employee.param.ts
+++ src/domain/params/employee.param.ts
@@ -1,15 +1,49 @@
-import { z } from 'zod';
-import { EmployeeIdSchema } from '../models/employee.model';
+import { z } from "zod";
+import {
+  EmployeeAgeSchema,
+  EmployeeIdSchema,
+  EmployeeNameSchema,
+  EmployeeProfileImageSchema,
+  EmployeeSalarySchema,
+} from "../models/employee.model";
 
-
-
-export const GetEmployeeByIdSchema = z.object({
-    'id': EmployeeIdSchema,
+// ID
+export const EmployeeByIdSchema = z.object({
+  id: EmployeeIdSchema.optional(),
 });
+export type EmployeeByIdParams = z.infer<typeof EmployeeByIdSchema>;
+
+// CREATE
+export const CreateEmployeeDataSchema = z.object({
+  name: EmployeeNameSchema,
+});
+export type CreateEmployeeDataParams = z.infer<typeof CreateEmployeeDataSchema>;
+
+export const CreateEmployeeSchema = z.object({
+  data: CreateEmployeeDataSchema,
+});
+export type CreateEmployeeParams = z.infer<typeof CreateEmployeeSchema>;
+
+// GET
+export const GetEmployeeByIdSchema = EmployeeByIdSchema;
 export type GetEmployeeByIdParams = z.infer<typeof GetEmployeeByIdSchema>;
 
-export const UpdateEmployeeSchema = z.object({
-    'name': z.string().min(1),
+// UPDATE
+export const UpdateEmployeeDataSchema = z.object({
+  name: EmployeeNameSchema,
+  salary: EmployeeSalarySchema,
+  age: EmployeeAgeSchema,
+  profile_image: EmployeeProfileImageSchema,
 });
+export type UpdateEmployeeDataParams = z.infer<typeof UpdateEmployeeDataSchema>;
+
+export const UpdateEmployeeSchema = z
+  .object({
+    data: UpdateEmployeeDataSchema,
+  })
+  .merge(EmployeeByIdSchema);
 export type UpdateEmployeeParams = z.infer<typeof UpdateEmployeeSchema>;
 
+// DELETE
+export const DeleteEmployeeByIdSchema = EmployeeByIdSchema;
+export type DeleteEmployeeByIdParams = z.infer<typeof DeleteEmployeeByIdSchema>;
